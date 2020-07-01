a77bb90c3a56e8d8980efb6d8e583ce0bc70559f
Ramadhan1012 <62045674+Ramadhan1012@users.noreply.github.com>
2020-07-01 19:46:59 +0700

    ClassAnak

---
+++ KaryawanTetap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TugasLab9_OOP_Collection
+{
+    public class KaryawanTetap : Karyawan
+    {
+        public double GajiBulanan { get; set; }
+        public override double Gaji()
+        {
+            return GajiBulanan;
+        }
+    }
+}
---
+++ Sales.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TugasLab9_OOP_Collection
+{
+    public class Sales : Karyawan
+    {
+        public int JumlahPenjualan { get; set; }
+        public int Komisi { get; set; }
+        public override double Gaji()
+        {
+            return JumlahPenjualan * Komisi;
+        }
+    }
+}
