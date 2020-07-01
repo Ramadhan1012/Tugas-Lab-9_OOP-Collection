6adc1b14037f10c8b481832c53b025b6445deb1d
Ramadhan1012 <62045674+Ramadhan1012@users.noreply.github.com>
2020-07-01 19:55:45 +0700

    Add files via upload

---
+++ ClassAnak/KaryawanHarian.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TugasLab9_OOP_Collection
+{
+    public class KaryawanHarian : Karyawan
+    {
+        public int JumlahJamKerja { get; set; }
+        public int UpahPerJam { get; set; }
+        public override double Gaji()
+        {
+            return JumlahJamKerja * UpahPerJam;
+        }
+    }
+}
